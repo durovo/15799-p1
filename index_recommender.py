3a3e6e249134d6daa7544c86f347f7b997a017e0
Dhruv Arya <aryadhruv@gmail.com>
2022-03-16 18:37:00 -0400

    Switch to picking index combinations randomly

--- index_recommender.py
+++ index_recommender.py
@@ -4,8 +4,23 @@ from sql_metadata import Parser
 from collections import defaultdict
 from itertools import combinations
 import psycopg2 as pg
+from pglast import parser
+from random import shuffle, randint, choice
 
 CONNECTION_STRING = "dbname='project1db' user='project1user' password='project1pass' host='localhost'"
+MAX_WIDTH = 3
+
+
+def get_table_column_map(conn):
+    QUERY = "select table_name, column_name from information_schema.columns where table_schema='public';"
+
+    table_columns_map = defaultdict(set)
+    with conn.cursor() as cursor:
+        cursor.execute(QUERY)
+        db_columns = cursor.fetchall()
+        for table, column in db_columns:
+            table_columns_map[table].add(column)
+    return table_columns_map
 
 
 def filter_interesting_queries(queries):
@@ -13,7 +28,7 @@ def filter_interesting_queries(queries):
            for q in queries if q.startswith('statement')]
     res = [q for q in res if 'pg_' not in q and not q.startswith('SHOW ALL') and not q.startswith(
         'COMMIT') and not q.startswith('SET') and not q.startswith('BEGIN')]
-    res = [q for q in res if 'WHERE' in q or 'ORDER' in q or 'JOIN' in q or 'join' in q]
+    res = [q for q in res if 'WHERE' in q or 'ORDER' in q or 'JOIN' in q or 'join' in q or 'where' in q or 'order' in q]
     return res
 
 
@@ -23,7 +38,7 @@ def cluster_queries(queries):
     gqueries = set()
     for query in queries:
         try:
-            generalized = Parser(query).generalize
+            generalized = parser.fingerprint(query)
             group_counts[generalized] += 1
             group_repr[generalized] = query
             gqueries.add(generalized)
@@ -32,57 +47,81 @@ def cluster_queries(queries):
     return gqueries, group_counts, group_repr
 
 
-def get_relevant_columns(clusters, cluster_counts):
-    column_usage_counts = defaultdict(int)
-    tables = []
-    table_columns = defaultdict(set)
-
+def get_relevant_columns(clusters, cluster_counts, table_column_map, max_width):
+    table_column_combos = defaultdict(set)
     for qgroup, query in clusters.items():
         try:
             pq = Parser(query)
-            tables.extend(pq.tables)
             columns = pq.columns_dict
+            pq_tables = pq.tables
             groups = ['where', 'order_by', 'join']
             for group in groups:
                 if group in columns:
+                    group_table_columns = defaultdict(set)
                     for col in columns[group]:
-                        if '.' in col:
-                            column_usage_counts[col] += cluster_counts[qgroup]
-                            tab, c = col.split('.')
-                            table_columns[tab].add(c)
+                        if '.' not in col:
+                            for table in pq_tables:
+                                if table in table_column_map and col in table_column_map[table]:
+                                    col = table + '.' + col
+                                    break
+                        tab, c = col.split('.')
+                        group_table_columns[tab].add(col)
+                    for tab, cols in group_table_columns.items():
+                        for width in range(1, min(len(cols)+1, max_width+1)):
+                            for col_combo in combinations(cols, width):
+                                table_column_combos[tab].add(col_combo)
         except:
             pass
-    tables = set(tables)
-#     print(table_columns)
-    ordered_columns = list(
-        reversed(sorted(list(column_usage_counts.items()), key=lambda x: x[1])))
-#     print(ordered_columns)
-    return ordered_columns
+    return table_column_combos
 
 
-def get_combinations_list(ordered_columns):
+def get_potential_indexes(table_column_combos):
+    potential_indexes = []
+    for v in table_column_combos.values():
+        potential_indexes.extend(v)
+    return potential_indexes
+
+
+def get_combinations_list(potential_indexes):
     combs = []
-    for i in range(1, len(ordered_columns)+1):
-        combs.extend(combinations(ordered_columns, i))
+    for i in range(1, len(potential_indexes)+1):
+        combs.extend(combinations(potential_indexes, i))
     return combs
 
 
-def get_columns_from_logs(logs_path):
+def get_random_indexes(potential_indexes):
+    num_indexes = randint(1, len(potential_indexes))
+    indexes_set = set()
+    while len(indexes_set) < num_indexes:
+        indexes_set.add(choice(potential_indexes))
+    return indexes_set
+
+
+def get_columns_from_logs(logs_path, max_width=2):
     QCOL = 13
     df = pd.read_csv(logs_path, header=None)
     queries = filter_interesting_queries(df[QCOL].tolist())
-    queries
+    with pg.connect(CONNECTION_STRING) as conn:
+        table_column_map = get_table_column_map(conn)
+#         print(table_column_map)
     gqueries, group_counts, group_repr = cluster_queries(queries)
-    cols = get_relevant_columns(group_repr, group_counts)
-    return cols, group_counts, group_repr
+    table_column_combos = get_relevant_columns(
+        group_repr, group_counts, table_column_map, max_width=MAX_WIDTH)
+    return table_column_combos, group_counts, group_repr
 
 
 def generate_index_creation_queries(columns):
     query_template = 'CREATE INDEX ON {} ({})'
     queries = []
-    for column in columns:
-        tab, col = column[0].split('.')
-        queries.append(query_template.format(tab, col))
+
+    for column_group in columns:
+
+        table_name = column_group[0].split('.')[0]
+
+        columns = [column.split('.')[1] for column in column_group]
+
+        queries.append(query_template.format(table_name, ', '.join(columns)))
+
     return queries
 
 
@@ -123,38 +162,37 @@ def get_query_costs(query_clusters, conn):
     return costs
 
 
-def find_best_index(log_file_path):
-    with open('actions.sql', 'w') as f:
-        f.write('Select 13 from review;')
-    cols, group_counts, group_repr = get_columns_from_logs(
+def find_best_index(log_file_path, max_iterations=3000):
+    table_column_combos, group_counts, group_repr = get_columns_from_logs(
         log_file_path)
-    cmbns = get_combinations_list(cols)
     best_config = []
-    with pg.connect("dbname='project1db' user='project1user' password='project1pass' host='localhost'") as conn:
+    with open('config.json', 'w') as f:
+        f.write('{"VACUUM": false}')
+    with pg.connect(CONNECTION_STRING) as conn:
         baseline_costs = get_query_costs(group_repr, conn)
         baseline_cost = get_scaled_loss(group_counts, baseline_costs)
-        baseline_cost
         min_cost = baseline_cost
-        best_config = []
         enable_hypopg(conn)
-        for cmb in cmbns:
+        potential_indexes = get_potential_indexes(table_column_combos)
+        for i in range(max_iterations):
+            cmb = get_random_indexes(potential_indexes)
             index_q = generate_index_creation_queries(cmb)
             create_hypothetical_indexes(index_q, conn)
             costs = get_query_costs(group_repr, conn)
             cost = get_scaled_loss(group_counts, costs)
             remove_hypo_indexes(conn)
-            print(cmb, cost)
             if cost < min_cost or (cost == min_cost and len(cmb) < len(best_config)):
                 min_cost = cost
                 best_config = cmb
 
+            if i % 1000 == 0:
+                #                 print(best_config, min_cost, baseline_cost)
+                with open('actions.sql', 'w') as f:
+                    for query in index_q:
+                        f.write("{};\n".format(query))
+
     index_creation_queries = generate_index_creation_queries(best_config)
     with open('actions.sql', 'w') as f:
         for query in index_creation_queries:
-            
-            f.write("{};\n".format(query))
-    with open('config.json', 'w') as f:
-        f.write('{"VACUUM": false}')
 
-    
-    
+            f.write("{};\n".format(query))
