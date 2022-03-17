02e5fc72c38882bf8939747aa4f38f06cfba1d23
Dhruv Arya <aryadhruv@gmail.com>
2022-03-16 21:01:33 -0400

    ensure that pip gets installed

--- dodo.py
+++ dodo.py
@@ -26,7 +26,8 @@ def task_project1_setup():
     return {
         "actions": [
             'sudo apt update',
-            'pip install -r requirements.txt',
+            'sudo apt install python3-pip'
+            'pip3 install -r requirements.txt',
             'sudo apt install postgresql-14-hypopg'
         ]
     }
